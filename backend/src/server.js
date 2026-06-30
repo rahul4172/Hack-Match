8d7de3e9ea160f4358798bdf611170e8114cf6c8
rahul4172 <rahul44484448@gmail.com>
2026-06-30 13:49:57 +0530

    Fix ENOENT error by replacing static html serving with API 404 response

--- backend/src/server.js
+++ backend/src/server.js
@@ -666,7 +666,10 @@ app.get('/stories/:id', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', (req, res) => {
+  res.json({ message: 'HackMatch API is running' });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Endpoint not found' });
 });
